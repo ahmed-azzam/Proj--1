46bd3da3efd9e6611c3ec51f4036e5763a500d9d
ahmed-azzam <ahmede.azzam@gmail.com>
2020-05-17 20:56:37 +0300

    v1.2

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+
 var stor = $("#stor")
-
-var hrM = $("#month");
-var pay = $("#pay");
-
-
-var data =[];
-var empObj= {};
+  var data =[];
 //name,salary,ct200,ct100,ct50,ct20,ct10,ct5,ct2,ct1
 
 stor.click( function() {
+   
+   var empObj= {};
 	var name = $("#EmpName").val();
 	var hrM = $("#month").val();
 	var pay = $("#pay").val();
+if (name ==="" || hrM ==="" || pay ===""){
+	alert("Please fill all fields");
+}else{
 
 	var salary = hrM*pay;
 	var c100 = salary%200;
@@ -31,5 +31,37 @@ stor.click( function() {
 	var ct2 = Math.floor(c2/2);
 	var ct1 = Math.floor(c1/1);
 
+	empObj["name"]=name; 
+	empObj["salary"]=salary;
+	empObj["ct200"]=ct200;
+	empObj["ct100"]=ct100;
+	empObj["ct50"]=ct50;
+	empObj["ct20"]=ct20;
+	empObj["ct10"]=ct10;
+	empObj["ct5"]=ct5;
+	empObj["ct2"]=ct2;
+	empObj["ct1"]=ct1;
+    data.push(empObj);
+   
+    //var names = $("#names");
+   // names.html("The employee: "+name + " is added") ;
+
+     var names=$("<p></p>");
+     var nameLi=$("#first-view");
+     nameLi.append(names);
+     names.text("The employee: "+name + " is added");
+
+      $("#EmpName").val("");
+      $("#month").val("");
+	  $("#pay").val("");
+
+	console.log(data);
+
+	}
+});
+
+var clear =$("#cl");
+cl.click ( function (){
+	data = [];
 });
 
